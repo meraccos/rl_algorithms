b9827ad6b6abdd41a53452dae98101855de89c0a
Meraj Mammadov <meraccos@gmail.com>
2023-09-12 10:48:48 +0900

    observation normalization speed up

--- sac/train.py
+++ sac/train.py
@@ -2,14 +2,22 @@ import torch
 import torch.nn.functional as F
 
 import gymnasium as gym
+from gymnasium.wrappers import TransformObservation
 from tqdm import tqdm
 from model import ReplayBuffer, Actor, Critic
 from torch.nn.utils import clip_grad_norm_
 from torch.utils.tensorboard import SummaryWriter
 writer = SummaryWriter()
     
-# env = gym.make("LunarLander-v2", continuous=True, render_mode="rgb_array")
+# Make the environment and normalize the obs
 env = gym.make('LunarLanderContinuous-v2', render_mode="rgb_array")
+
+obs_a = 2 / (env.observation_space.high - env.observation_space.low)
+obs_b = ((env.observation_space.high + env.observation_space.low) / 
+          (env.observation_space.high - env.observation_space.low))
+
+env = TransformObservation(env, lambda obs: obs * obs_a - obs_b)
+
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 actor = Actor(env).to(device)
@@ -22,16 +30,16 @@ Q2_ = Critic(env).to(device)
 Q1_.load_state_dict(Q1.state_dict())
 Q2_.load_state_dict(Q2.state_dict())
 
-optim_a = torch.optim.Adam(actor.parameters(), lr=0.0001)
-optim_q1 = torch.optim.Adam(Q1.parameters(), lr=0.0003)
-optim_q2 = torch.optim.Adam(Q2.parameters(), lr=0.0003)
+optim_a = torch.optim.Adam(actor.parameters(), lr=0.00005)
+optim_q1 = torch.optim.Adam(Q1.parameters(), lr=0.0005)
+optim_q2 = torch.optim.Adam(Q2.parameters(), lr=0.0005)
 
 # Hyperparameters
 gamma = 0.99
 temp = 0.0003     # Temperature of the entropy term
 train_ep_freq = 1
 num_opt_steps = 1
-eval_freq = 20
+eval_freq = 30
 num_eval_ep = 10
 start_steps = 50000
 clip_value = 0.5
@@ -52,7 +60,6 @@ def soft_update(target, source, tau=0.01):
 def run_episode(test=False, render=False, random=False):
     global global_step
     obs, _ = env.reset()
-    obs = (obs - env.observation_space.low) / (env.observation_space.high - env.observation_space.low) * 2 -1
     done = False
     rews_ep = []
 
@@ -63,11 +70,10 @@ def run_episode(test=False, render=False, random=False):
         if random:
             action = env.action_space.sample()
         else:
-            action = actor.sample(torch.tensor(obs).unsqueeze(0).to(device), test=test)[0]
-            action = action[0].to('cpu').detach().numpy()
+            action = actor.sample(torch.tensor(obs).unsqueeze(0).to(device), test=test)
+            action = action[0][0].to('cpu').detach().numpy()
     
         obs_, reward, terminated, truncated, _ = env.step(action)
-        obs_ = (obs_ - env.observation_space.low) / (env.observation_space.high - env.observation_space.low) * 2 -1
         done = terminated or truncated
         
         if not truncated:
@@ -109,13 +115,11 @@ def optimize(episode):
     
     actor.zero_grad()
     loss_a.backward()
-    # break
     clip_grad_norm_(actor.parameters(), clip_value)
     optim_a.step()
     
-    writer.add_scalar("Loss/q1", loss_q1.cpu().detach(), episode)
-    writer.add_scalar("Loss/q2", loss_q2.cpu().detach(), episode)
-    writer.add_scalar("Loss/a", loss_a.cpu().detach(), episode)
+    writer.add_scalar("Loss/Critic", loss_q1.cpu().detach(), episode)
+    writer.add_scalar("Loss/Actor", loss_a.cpu().detach(), episode)
     writer.add_scalar("Loss/entropy", -temp * log_prob.mean(), episode)
     
     soft_update(Q1_, Q1, tau=0.01)
