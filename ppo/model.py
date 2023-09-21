1f35340d70e88d2924a2381b1abf87b82870c43a
Meraj Mammadov <meraccos@gmail.com>
2023-09-21 19:57:15 +0900

    ppo works

--- ppo/model.py
+++ ppo/model.py
@@ -1,9 +1,67 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import torch.distributions as pyd
 from torch.distributions.normal import Normal
 
+import numpy as np
+import math
 
+
+class TanhTransform(pyd.transforms.Transform):
+    domain = pyd.constraints.real
+    codomain = pyd.constraints.interval(-1.0, 1.0)
+    bijective = True
+    sign = +1
+
+    def __init__(self, cache_size=1):
+        super().__init__(cache_size=cache_size)
+
+    @staticmethod
+    def atanh(x):
+        return 0.5 * (x.log1p() - (-x).log1p())
+
+    def __eq__(self, other):
+        return isinstance(other, TanhTransform)
+
+    def _call(self, x):
+        return x.tanh()
+
+    def _inverse(self, y):
+        # We do not clamp to the boundary here as it may degrade the performance of certain algorithms.
+        # one should use `cache_size=1` instead
+        return self.atanh(y)
+
+    def log_abs_det_jacobian(self, x, y):
+        # We use a formula that is more numerically stable, see details in the following link
+        # https://github.com/tensorflow/probability/commit/ef6bb176e0ebd1cf6e25c6b5cecdd2428c22963f#diff-e120f70e92e6741bca649f04fcd907b7
+        return 2. * (math.log(2.) - x - F.softplus(-2. * x))
+
+class SquashedNormal(pyd.transformed_distribution.TransformedDistribution):
+    def __init__(self, loc, scale):
+        self.loc = loc
+        self.scale = scale
+
+        self.base_dist = pyd.Normal(loc, scale)
+        transforms = [TanhTransform()]
+        super().__init__(self.base_dist, transforms)
+
+    @property
+    def mean(self):
+        mu = self.loc
+        for tr in self.transforms:
+            mu = tr(mu)
+        return mu
+
+def initialize_weights(model):
+    for m in model.modules():
+        if isinstance(m, nn.Linear):
+            nn.init.xavier_uniform_(m.weight)
+            # nn.init.kaiming_uniform_(m.weight, nonlinearity='relu')
+            if m.bias is not None:
+                nn.init.constant_(m.bias, 0)
+                
+                
 class Actor(nn.Module):
     def __init__(self, env=None):
         super().__init__()
@@ -13,35 +71,38 @@ class Actor(nn.Module):
         self.l1 = nn.Linear(self.input_dim, 64)
         self.l2 = nn.Linear(64, 64)
         self.l3 = nn.Linear(64, self.output_dim * 2)
+        
+        initialize_weights(self)
 
     def forward(self, x):
-        a = F.relu(self.l1(x))
-        a = F.relu(self.l2(a))
-        mu, log_std = self.l3(a).chunk(2, dim=-1)
-        return mu, log_std
+        a = F.leaky_relu(self.l1(x), 0.1)
+        a = F.leaky_relu((self.l2(a)), 0.1)
+        mu, std = self.l3(a).chunk(2, dim=-1)
+        std = torch.sigmoid(std)
+        std = 7.0 * std + 1e-8
+
+        return mu, std
     
-    def sample(self, x, test=False):
-        mu, log_std = self(x)
-        if test:
-            action = mu
-            logp = None
+    def dist(self, x):
+        mu, std = self(x)
+        n = Normal(mu, std)
+        # n = SquashedNormal(mu, std)
+        return n
         
-        else:
-            std = torch.exp(log_std)
-            n = Normal(mu, std)
-            action = n.rsample().tanh()
-            logp = (n.log_prob(action) - torch.log(1 - action.pow(2) + 1e-6)).sum(1)
+    def sample(self, x, deterministic = False):
+        if deterministic:
+            return self(x)[0].tanh(), 0
+        dist = self.dist(x)
+        action = dist.rsample().tanh()
+        logp = (dist.log_prob(action) - torch.log(1 - action.pow(2) + 1e-6)).sum(1)
         
         return action, logp
     
     def logp(self, x, action):
-        mu, log_std = self(x)
-        std = torch.exp(log_std)
-        n = Normal(mu, std)
-        logp = (n.log_prob(action) - torch.log(1 - action.pow(2) + 1e-6)).sum(1)
-        
+        dist = self.dist(x)
+        logp = (dist.log_prob(action) - torch.log(1 - action.pow(2) + 1e-6)).sum(1)
         return logp
-
+                    
     
 class Critic(nn.Module):
     def __init__(self, env=None):
@@ -51,47 +112,185 @@ class Critic(nn.Module):
         self.l1 = nn.Linear(self.input_dim, 64)
         self.l2 = nn.Linear(64, 64)
         self.l3 = nn.Linear(64, 1)
+        
+        initialize_weights(self)
 
     def forward(self, x):
-        x = F.relu(self.l1(x))
-        x = F.relu(self.l2(x))
+        x = F.leaky_relu((self.l1(x)), 0.1)
+        x = F.leaky_relu((self.l2(x)), 0.1)
         x = self.l3(x)
         return x.squeeze()
 
-
-class TrajectoryStorage:
+class Buffer:
+    '''
+    '''
     def __init__(self, device='cpu'):
         self.device = device
-
-        self.states = []
-        self.actions = []
-        self.logp = []
-        self.rewards = []
-        self.next_states = []
-        self.dones = []
+        self.dtype = torch.float32
+        self.reset_all()
     
     def store(self, s, a, lp, r, s_, d):
+        '''
+        Stores the returns from each step into the episode memory
+        '''
         self.states.append(s)
         self.actions.append(a)
         self.logp.append(lp)
         self.rewards.append(r)
-        self.next_states.append(s_)
+        self.states_.append(s_)
         self.dones.append(int(d))
-
-    def load(self):
-        states = torch.tensor(self.states, device=self.device, dtype=torch.float32)
-        actions = torch.tensor(self.actions, device=self.device, dtype=torch.float32)
-        logp = torch.tensor(self.logp, device=self.device, dtype=torch.float32)
-        rewards = torch.tensor(self.rewards, device=self.device, dtype=torch.float32)
-        next_states = torch.tensor(self.next_states, device=self.device, dtype=torch.float32)
-        dones = torch.tensor(self.dones, device=self.device, dtype=torch.float32)
-        
-        return states, actions, logp, rewards, next_states, dones
     
-    def reset(self):
+    def store_adv_and_rets(self, advantages, returns):
+        self.advantages += advantages
+        self.returns += returns
+            
+    def load_ep(self):
+        '''
+        Returns s, a, lp, r, s_, d of the last finished episode
+        '''
+        states = torch.tensor(np.array(self.states), device=self.device, dtype=self.dtype)
+        actions = torch.tensor(np.array(self.actions), device=self.device, dtype=self.dtype)
+        logp = torch.tensor(np.array(self.logp), device=self.device, dtype=self.dtype)
+        rewards = torch.tensor(np.array(self.rewards), device=self.device, dtype=self.dtype)
+        states_ = torch.tensor(np.array(self.states_), device=self.device, dtype=self.dtype)
+        dones = torch.tensor(np.array(self.dones), device=self.device, dtype=self.dtype)
+        advs = torch.tensor(np.array(self.advantages), device=self.device, dtype=self.dtype)
+        rets = torch.tensor(np.array(self.returns), device=self.device, dtype=self.dtype)
+        
+        return states, actions, logp, rewards, states_, dones, advs, rets
+
+    def load_state_and_rewards(self):
+        '''
+        Returns s, r of the last finished episode
+        '''
+        states = torch.tensor(np.array(self.states), device=self.device, dtype=self.dtype)
+        rewards = torch.tensor(np.array(self.rewards), device=self.device, dtype=self.dtype)
+        
+        return states, rewards
+    
+    def load_all(self, batch_size=None, ith=0):
+        '''
+        Returns logs of all the episodes in the batch
+        '''
+        size_frac = (self.all_actions.shape[0] // batch_size)
+        ith = ith % size_frac
+        indices = self.shuffled_indices[ith*batch_size:(ith+1)*batch_size]
+
+        return (self.all_states[indices], 
+                self.all_actions[indices], 
+                self.all_logp.squeeze()[indices], 
+                self.all_rewards.squeeze()[indices], 
+                self.all_states_[indices], 
+                self.all_dones.squeeze()[indices],
+                self.all_advs.squeeze()[indices],
+                self.all_returns.squeeze()[indices])
+  
+    def discounted_rewards(self, r, gamma):
+        '''
+        Returns the discounted rewards based on the input array of rewards, r
+        '''
+        t_steps = torch.arange(len(r)).to(self.device)
+        r_gamma = r * gamma ** t_steps
+        r_disc = reversed(reversed(r_gamma).cumsum(0)) / gamma ** t_steps
+        return r_disc
+    
+    def save_ep(self):
+        '''
+        Loads all the episodic buffer to the batch buffer. 
+        Call at the end of each episode.
+        '''
+        s, a, lp, r, s_, d, adv, ret = self.load_ep() 
+        self.all_states = torch.vstack((self.all_states, s))
+        self.all_actions = torch.vstack((self.all_actions, a))
+        self.all_logp = torch.vstack((self.all_logp, lp))
+        self.all_rewards = torch.vstack((self.all_rewards, r.view(-1, 1)))
+        self.all_states_ = torch.vstack((self.all_states_, s_))
+        self.all_dones = torch.vstack((self.all_dones, d.view(-1, 1)))
+        self.all_advs = torch.vstack((self.all_advs, adv.view(-1, 1)))
+        self.all_returns = torch.vstack((self.all_returns, ret.view(-1, 1)))
+        
+        self.shuffled_indices = torch.randperm(self.all_states.shape[0])
+    
+    def reset_ep(self):     
+        '''
+        Erases the episodic buffer. 
+        Call at the beginning of each episode.
+        '''   
         self.states = []
         self.actions = []
         self.logp = []
         self.rewards = []
-        self.next_states = []
+        self.states_ = []
         self.dones = []
+        self.advantages = []
+        self.returns = []
+        
+    def reset_all(self):
+        '''
+        Erases both the episodic buffer and batch buffer. 
+        Call after one optimization session is done.
+        '''
+        self.reset_ep()
+        
+        # Batch of episodes memory, concated
+        self.all_states = torch.empty((0, 8), device=self.device, dtype=self.dtype)
+        self.all_actions = torch.empty((0, 2), device=self.device, dtype=self.dtype)
+        self.all_logp = torch.empty((0, 1), device=self.device, dtype=self.dtype)
+        self.all_rewards = torch.empty((0, 1), device=self.device, dtype=self.dtype)
+        self.all_states_ = torch.empty((0, 8), device=self.device, dtype=self.dtype)
+        self.all_dones = torch.empty((0, 1), device=self.device, dtype=self.dtype)
+        self.all_advs = torch.empty((0, 1), device=self.device, dtype=self.dtype)
+        self.all_returns = torch.empty((0, 1), device=self.device, dtype=self.dtype)
+
+
+
+# class Buffer:
+#     def __init__(self, device='cuda'):
+#         self.device = device
+#         self.dtype = torch.float32
+#         self.reset()
+
+#     def reset(self):     
+#         self.states = []
+#         self.actions = []
+#         self.logp = []
+#         self.rewards = []
+#         self.states_ = []
+#         self.dones = []
+#         self.advantages = []
+        
+#     def store(self, s, a, lp, r, s_, d):
+#         self.states.append(s)
+#         self.actions.append(a)
+#         self.logp.append(lp)
+#         self.rewards.append(r)
+#         self.states_.append(s_)
+#         self.dones.append(int(d))
+    
+#     def store_adv(self, adv):
+#         self.advantages.append(adv)
+
+#     def load_all(self):
+#         states = torch.tensor(np.array(self.states), device=self.device, dtype=self.dtype)
+#         actions = torch.tensor(np.array(self.actions), device=self.device, dtype=self.dtype)
+#         logp = torch.tensor(np.array(self.logp), device=self.device, dtype=self.dtype)
+#         rewards = torch.tensor(np.array(self.rewards), device=self.device, dtype=self.dtype)
+#         states_ = torch.tensor(np.array(self.states_), device=self.device, dtype=self.dtype)
+#         dones = torch.tensor(np.array(self.dones), device=self.device, dtype=self.dtype)
+#         advantages = torch.tensor(np.hstack(self.advantages), device=self.device, dtype=self.dtype)
+        
+#         return states, actions, logp.squeeze(), rewards, states_, dones, advantages
+
+#     def get_batch(self, batch_size):
+#         s, a, lp, r, s_, d, advantages = self.load_all() 
+#         init_idx = random.randint(0, s.shape[0] - batch_size)
+#         end_idx = init_idx + batch_size
+        
+#         return (s[init_idx:end_idx],
+#                 a[init_idx:end_idx],
+#                 lp[init_idx:end_idx],
+#                 r[init_idx:end_idx],
+#                 s_[init_idx:end_idx],
+#                 d[init_idx:end_idx],
+#                 advantages[init_idx:end_idx])
+
---
+++ ppo/ppo.py
@@ -0,0 +1,186 @@
+import torch 
+import torch.nn.functional as F
+from torch.utils.tensorboard import SummaryWriter
+
+import gymnasium as gym
+from gymnasium.wrappers import TransformObservation
+
+from tqdm import tqdm
+import numpy as np
+
+from model import Actor, Critic, Buffer
+
+class PPO:
+    def __init__(self):
+        self.env = self.get_env()
+        self.writer = SummaryWriter()
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.buffer = Buffer(self.device)
+
+        self.gamma = 0.99
+        self.lam = 0.97
+        self.temp = 0.02
+        self.eps_clip = 0.1
+        self.buffer_size = 2048
+        self.mini_batch = 256
+        batches_in_buffer = self.buffer_size // self.mini_batch
+        self.num_epoch = batches_in_buffer * 10
+        self.step = 0
+        self.actor_lr = 1e-4
+        self.critic_lr = 1e-3
+        self.get_models()
+
+        self.running_reward_std = 1.0
+        
+    def get_env(self):
+        env = gym.make("LunarLanderContinuous-v2")       
+        obs_high, obs_low = env.observation_space.high, env.observation_space.low
+        obs_a = 2 / (obs_high - obs_low)
+        obs_b = (obs_high + obs_low) / (obs_high - obs_low)
+        env = TransformObservation(env, lambda obs: obs * obs_a - obs_b)
+        return env
+    
+    def get_models(self):
+        self.actor = Actor(self.env).to(self.device)
+        self.critic = Critic(self.env).to(self.device)
+        self.optim_a = torch.optim.AdamW(self.actor.parameters(), lr=self.actor_lr, weight_decay=0.0001)
+        self.optim_v = torch.optim.AdamW(self.critic.parameters(), lr=self.critic_lr, weight_decay=0.0001)
+    
+    def normalize(self, x):
+        return (x - x.mean()) / (x.std() + 1e-8)
+
+    @torch.no_grad()
+    def compute_gae(self, rewards, values, terminal_value=0.0):
+        values = torch.cat([values, torch.tensor([terminal_value], device=self.device)])
+        # Calculate the temporal differences
+        deltas = rewards + self.gamma * values[1:] - values[:-1]
+
+        # Compute GAE advantages
+        advantages = torch.zeros_like(rewards, device=self.device)
+        gae = 0
+        for t in reversed(range(len(rewards))):
+            gae = deltas[t] + self.gamma * self.lam * gae
+            advantages[t] = gae
+
+        returns = advantages + values[:-1]
+        # returns = self.buffer.discounted_rewards(rewards, self.gamma)
+        return advantages, returns
+
+    def update_running_reward_std(self, new_rewards):
+        # Welford's algorithm
+        n = len(new_rewards)
+        if n == 0:
+            return
+        new_mean = new_rewards.mean()
+        new_std = new_rewards.std()
+        self.running_reward_std = np.sqrt(((self.running_reward_std ** 2) + (new_std ** 2)) / 2)
+
+    def scale_rew(self, r):
+        self.update_running_reward_std(r.cpu().numpy())
+        scaled_r = r / self.running_reward_std
+        return scaled_r
+        
+    def optimize(self):
+        actor_losses = []
+        critic_losses = []
+        kl_divs = []
+        for epoch in range(self.num_epoch):
+            s, a, lp, r, s_, d, advantages, returns = self.buffer.load_all(self.mini_batch, ith=epoch)
+            advantages = self.normalize(advantages)
+            
+            # Actor loss
+            logp = self.actor.logp(s, a)
+            ratios = torch.exp(logp - lp)   
+            kl_div = (logp - lp).mean().cpu().detach()
+            if abs(kl_div) > 1.0:
+                print('early stopping at ', epoch)
+                break
+            surr1 = ratios * advantages            
+            surr2 = torch.clamp(ratios, 1-self.eps_clip, 1+self.eps_clip) * advantages  
+            loss_policy = -torch.min(surr1, surr2).mean() - self.temp * logp.mean()
+
+            loss_value = F.mse_loss(self.critic(s), returns)                           
+            
+            # Actor Network
+            self.optim_a.zero_grad()
+            loss_policy.backward()
+            self.optim_a.step()
+
+            # Critic network
+            self.optim_v.zero_grad()
+            loss_value.backward()
+            self.optim_v.step()   
+            
+            actor_losses.append(loss_policy.cpu().detach())
+            critic_losses.append(loss_value.cpu().detach()) 
+            kl_divs.append(kl_div)
+
+        self.writer.add_scalar('Train/critic_loss', sum(critic_losses)/self.num_epoch, self.step)
+        self.writer.add_scalar('Train/actor_loss', sum(actor_losses)/self.num_epoch, self.step)
+
+        self.writer.add_scalar('Train/kl_div', sum(kl_divs)/self.num_epoch, self.step)
+    
+    def _get_action(self, obs):
+        obs = torch.tensor(obs).unsqueeze(0).to(self.device)
+        action, logp = self.actor.sample(obs)
+        action = action.detach().cpu().numpy()[0]
+        logp = logp.cpu().detach()
+        return action, logp
+    
+    def _save_advantages(self, states, rews, terminal_value=0.0):
+        values = self.critic(states).detach()
+        advs, returns = self.compute_gae(rews, values, terminal_value)
+        
+        self.buffer.store_adv_and_rets(advs.cpu().tolist(), returns.cpu().tolist())
+    
+    @torch.no_grad()
+    def collect_data(self):
+        obs, _ = self.env.reset()
+        rews = []
+        lengths = []
+
+        while True:
+            action, logp = self._get_action(obs)
+            obs_, reward, terminated, truncated, _ = self.env.step(action)
+            done = terminated or truncated
+            
+            self.buffer.store(obs, action, logp, reward, obs_, done)
+            self.step += 1
+            obs = obs_
+
+            if done:
+                s, r= self.buffer.load_state_and_rewards()                
+                scaled_r = self.scale_rew(r)
+                
+                self._save_advantages(s, scaled_r)           
+                self.buffer.save_ep()
+                self.buffer.reset_ep()
+                obs, _ = self.env.reset() 
+                rews.append(r.sum())
+                lengths.append(r.shape[0])
+                
+            if self.step == self.buffer_size:
+                s, r= self.buffer.load_state_and_rewards()
+                scaled_r = self.scale_rew(r)
+                
+                if s.shape[0] > 1:
+                    future_reward = self.critic(torch.tensor(obs).unsqueeze(0).to(self.device)).item()
+                    self._save_advantages(s, scaled_r, terminal_value=future_reward)
+                    self.buffer.save_ep()
+                    
+                self.buffer.reset_ep()
+                self.writer.add_scalar('Train/rews', sum(rews) / len(rews), self.step) 
+                self.writer.add_scalar('Train/length', sum(lengths) / len(lengths), self.step) 
+                break
+
+    def train(self, num_episodes=100000):
+        for episode in tqdm(range(num_episodes)):    
+            # Collect data
+            self.collect_data()
+            self.optimize()
+            self.buffer.reset_all()
+            
+if __name__ == '__main__':
+    ppo = PPO()
+    ppo.train(100000)
+    
